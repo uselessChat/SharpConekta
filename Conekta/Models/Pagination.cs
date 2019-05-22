da438360fd680e6e717142ef90e4592cb4cefa77
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 00:16:36 -0500

    Add model Pagination

---
+++ Conekta/Models/Pagination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public class Pagination
+    {
+        [JsonProperty("next_page_url")]
+        public string NextPage { get; set; }
+        [JsonProperty("previous_page_url")]
+        public string PreviousPage { get; set; }
+        [JsonProperty("has_more")]
+        public bool HasMore { get; set; }
+
+        [JsonProperty("data")]
+        public IEnumerable Data { get; set; }
+    }
+
+    public class Pagination<T> : Pagination
+    {
+        [JsonProperty("data")]
+        public new IEnumerable<T> Data { get; set; }
+    }
+}
