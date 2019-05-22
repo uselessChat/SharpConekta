512db8efeecfca0583ce81eba6d128b7c593c613
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 00:58:01 -0500

    Add model Customer

---
+++ Conekta/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public class Customer
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("corporate")]
+        public bool Corporate { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("payment_sources")]
+        public Pagination<PaymentSource> PaymentSources { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("plan_id")]
+        public string PlanId { get; set; }
+
+        [JsonProperty("shipping_contacts")]
+        public Pagination<ShippingContact> ShippingContacts { get; set; }
+
+        [JsonProperty("subscriptions")]
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+    }
+}
