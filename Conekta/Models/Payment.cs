07d7b89e00bfc4f33c5dc2f551a2494510ebb6dd
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 00:26:26 -0500

    Add model PaymentSource

---
+++ Conekta/Models/Payment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public enum PaymentSourceType
+    {
+        Card
+    }
+
+    public class PaymentSource
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("parent_id")]
+        public string CustomerId { get; set; }
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("created_at")]
+        public long CreatedAt { get; set; }
+
+        [JsonProperty("exp_month")]
+        public string ExpirationMonth { get; set; }
+
+        [JsonProperty("exp_year")]
+        public string ExpirationYear { get; set; }
+
+        [JsonProperty("last4")]
+        public string Last4Digits { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        public PaymentSource() { }
+
+        public PaymentSource(PaymentSourceType type)
+        {
+            Type = type.ToString().ToLower();
+        }
+    }
+
+}
