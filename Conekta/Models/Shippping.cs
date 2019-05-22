ca3d4a451882332f7565ff1d4dda4a48e22f4e8f
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 00:00:16 -0500

    Add model ShipppingAddress

---
+++ Conekta/Models/Shippping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public class ShipppingAddress
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("street2")]
+        public string InternalNumber { get; set; }
+        [JsonProperty("residential")]
+        public bool Residential { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("street1")]
+        public string StreetAndNumber { get; set; }
+        [JsonProperty("postal_code")]
+        public string ZipCode { get; set; }
+    }
+}
