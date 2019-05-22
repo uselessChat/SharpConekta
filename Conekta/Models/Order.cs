9a485459728fae2265964515fbd6f86cf834d0ba
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 01:18:28 -0500

    Add model Order

---
+++ Conekta/Models/Order.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public enum OrderPaymentStatus
+    {
+        Charged_Back,
+        Declined,
+        Expired,
+        Paid,
+        Partially_Refunded,
+        Pending_Payment,
+        Pre_Authorized,
+        Refunded,
+        Voided
+    }
+
+    public class Order
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        [JsonProperty("amount_refunded")]
+        public int AmountRefunded { get; set; }
+
+        [JsonProperty("charges")]
+        public IEnumerable<Charge> Charges { get; set; }
+
+        [JsonProperty("created_at")]
+        public long CreatedAt { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("customer_info")]
+        public OrderCustomer Customer { get; set; }
+
+        [JsonProperty("discount_lines")]
+        public IEnumerable<DiscountLine> DiscountLines { get; set; }
+
+        [JsonProperty("line_items")]
+        public IEnumerable<LineItem> LineItems { get; set; }
+
+        [JsonProperty("livemode")]
+        public bool Livemode { get; set; }
+
+        [JsonProperty("payment_status")]
+        public string PaymentStatus { get; set; }
+
+        [JsonProperty("shipping_contact")]
+        public ShippingContact ShippingContact { get; set; }
+
+        [JsonProperty("shipping_lines")]
+        public IEnumerable<ShippingLine> ShippingLines { get; set; }
+
+        [JsonProperty("tax_lines")]
+        public IEnumerable<TaxLine> TaxLines { get; set; }
+
+        [JsonProperty("updated_at")]
+        public long UpdatedAt { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("metadata")]
+        public object Metadata { get; set; }
+
+        public Order() { }
+
+        public Order(OrderPaymentStatus paymentStatus)
+        {
+            PaymentStatus = paymentStatus.ToString().ToLower();
+        }
+    }
+
+    public class Order<M> : Order
+    {
+        [JsonProperty("metadata")]
+        public new M Metadata { get; set; }
+    }
+
+    public class Order<L, S, T> : Order
+    {
+        [JsonProperty("line_items")]
+        public new IEnumerable<LineItem<L>> LineItems { get; set; }
+
+        [JsonProperty("shipping_lines")]
+        public new IEnumerable<ShippingLine<S>> ShippingLines { get; set; }
+
+        [JsonProperty("tax_lines")]
+        public new IEnumerable<TaxLine<T>> TaxLines { get; set; }
+    }
+
+    public class Order<L, S, T, M> : Order<L, S, T>
+    {
+        [JsonProperty("metadata")]
+        public new M Metadata { get; set; }
+    }
+}
