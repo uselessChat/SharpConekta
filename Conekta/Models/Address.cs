1fe5fd9cae7f14174b57ee95c819352eb8aacadc
Daniel Cantu Guajardo <e-dcantu@neoris.com>
2019-05-22 00:19:27 -0500

    Add model Address

---
+++ Conekta/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Conekta.Models
+{
+    public class Address
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("street2")]
+        public string InternalNumber { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("street1")]
+        public string StreetAndNumber { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+    }
+}
