fa43c269cb7aa03977efcb5c5e0d40de15d1a496
uselessChat <vill.okse@gmail.com>
2019-05-28 17:26:52 -0500

    Add Shipping Lines requests

---
+++ Conekta/Models/ShippingContact.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conekta.Models
+{
+    /// <summary>
+    /// Describes the customer's information for the shipment
+    /// </summary>
+    public class ShippingContact
+    {
+        /// <summary>
+        /// Unique identifier, assigned at random
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Shipping address
+        /// </summary>
+        [JsonProperty("address")]
+        public ShipppingAddress Address { get; set; }
+
+        /// <summary>
+        /// Adjacent streets to the shipping address. (optional)
+        /// </summary>
+        [JsonProperty("between_streets")]
+        public string BetweenStreets { get; set; }
+
+        /// <summary>
+        /// Contact's phone number
+        /// </summary>
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Contact's name. (optional)
+        /// </summary>
+        [JsonProperty("receiver")]
+        public string Receiver { get; set; }
+    }
+}
---
+++ Conekta/Models/ShippingLine.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conekta.Models
+{
+    /// <summary>
+    /// A Shipping Line describes the shipment details for an order such as
+    ///  the method, the amount, the carrier and the tracking number.
+    /// </summary>
+    public class ShippingLine
+    {
+        /// <summary>
+        /// Unique identifier, assigned at random
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The shipping cost, in cents
+        /// </summary>
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+
+        /// <summary>
+        /// Name of the carrier
+        /// </summary>
+        [JsonProperty("carrier")]
+        public string Carrier { get; set; }
+
+        /// <summary>
+        /// Shipping method. (optional)
+        /// </summary>
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("parent_id")]
+        public string OrderId { get; set; }
+
+        /// <summary>
+        /// Tracking number provided by the carrier. (optional)
+        /// </summary>
+        [JsonProperty("tracking_number")]
+        public string Tracking { get; set; }
+
+        /// <summary>
+        /// Hash containing additional information related to the shipping line. (optional)
+        /// </summary>
+        [JsonProperty("metadata")]
+        public object Metadata { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+    }
+
+    public class ShippingLine<T> : ShippingLine
+    {
+        /// <summary>
+        /// Hash containing additional information related to the shipping line. (optional)
+        /// </summary>
+        [JsonProperty("metadata")]
+        public new T Metadata { get; set; }
+    }
+}
