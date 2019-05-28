49d239cd45dbaed5eb4be9fd9aa0f480a155570a
uselessChat <vill.okse@gmail.com>
2019-05-28 17:47:17 -0500

    Add TaxLine requests

---
+++ Conekta/Api/Services/TaxLine.cs
@@ -0,0 +1,41 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conekta.Api.Services
+{
+    public class TaxLine : Client
+    {
+        public Task<T> AllAsync<T>(string orderId)
+        {
+            Segment = $"/orders/{orderId}/tax_lines";
+            return Request.GetJsonAsync<T>();
+        }
+
+        public Task<T> CreateAsync<T>(string orderId, T data)
+        {
+            Segment = $"/orders/{orderId}/tax_lines";
+            return Request.PostJsonAsync(data).ReceiveJson<T>();
+        }
+
+        public Task<T> DeleteAsync<T>(string orderId, string id)
+        {
+            Segment = $"/orders/{orderId}/tax_lines/{id}";
+            return Request.DeleteAsync().ReceiveJson<T>();
+        }
+
+        public Task<T> FindAsync<T>(string orderId, string id)
+        {
+            Segment = $"/orders/{orderId}/tax_lines/{id}";
+            return Request.GetJsonAsync<T>();
+        }
+
+        public Task<T> UpdateAsync<T>(string orderId, string id, T data)
+        {
+            Segment = $"/orders/{orderId}/tax_lines/{id}";
+            return Request.PutJsonAsync(data).ReceiveJson<T>();
+        }
+    }
+}
